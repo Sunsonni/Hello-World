d0fb5563223e1ed5307063ec03c03afdd4958f64
sunsonni <sonniemnguyen@gmail.com>
2024-08-31 22:38:15 -0500

    Initial Commit

---
+++ Hello World/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+//getting standard library. input output stream
+
+int main() {
+    std::cout << "Hello World\n";
+    //cout is character out NOT console out;
+    return 0;
+    /* 0 tells operating system that program is terminating correctly. Any other numbers are other terminations. */
+}
+
